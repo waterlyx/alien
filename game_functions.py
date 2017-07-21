5120619248a4a290777c81515bc9f0085caef2eb
刘业兴 <waterlyx@qq.com>
2017-07-21 11:40:19 +0800

    增加游戏难度增加模块

--- game_functions.py
+++ game_functions.py
@@ -51,6 +51,8 @@ def check_play_button(ai_settings, screen, stats, play_button, ship, aliens, bul
     """在玩家单击play按钮的时候的开始新的游戏"""
     button_clicked = play_button.rect.collidepoint(mouse_x, mouse_y)
     if button_clicked and not stats.game_active:
+        # 重置游戏设置
+        ai_settings.initialize_dynamic_settings()
         # 隐藏光标
         pygame.mouse.set_visible(False)
         # 重置游戏统计信息
@@ -102,6 +104,7 @@ def check_bullet_alien_collisions(ai_settings, screen, ship, aliens, bullets):
     collisions = pygame.sprite.groupcollide(bullets, aliens, True, True)
     if len(aliens) == 0:
         bullets.empty()
+        ai_settings.increase_speed()
         create_fleet(ai_settings, screen, ship, aliens)
 
 
--- settings.py
+++ settings.py
@@ -18,7 +18,23 @@ class Settings:
         self.bullet_color = 255, 255, 255
         self.bullets_allowed = 5
         # 外星人设置
-        self.alien_speed_factor = 0.4
+        self.alien_speed_factor = 0.2
         self.fleet_drop_speed = 10
         self.fleet_direction = 1
+        # 增加难度设置
+        self.speedup_scale = 1.1
+        self.initialize_dynamic_settings()
+
+    def initialize_dynamic_settings(self):
+        """初始化随游戏进行而变化的设置"""
+        self.ship_speed_factor = 1.5
+        self.bullet_speed_factor = 2
+        self.alien_speed_factor = 0.2
+        self.fleet_direction = 1
+
+    def increase_speed(self):
+        """提高速度设置"""
+        self.ship_speed_factor *= self.speedup_scale
+        self.bullet_speed_factor *= self.speedup_scale
+        self.alien_speed_factor *= self.speedup_scale
 
